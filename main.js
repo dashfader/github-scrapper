1307e0d70b4c67e58c3b2dd9538cfe604b6018b6
Fade-ing <fdlandriyanto@gmail.com>
2024-03-23 22:37:28 +0800

    main: Use try catch to handle username response

--- main.js
+++ main.js
@@ -3,24 +3,28 @@ const cheerio = require('cheerio')
 const { Input } = require('enquirer')
 
 const githubScrapping = async () => {
-    const userPrompt = new Input({
-        message: 'What is the github username?',
-        initial: 'Fade-ing'
-    })
-    const username = await userPrompt.run()
-    const response = await axios.get(`https://github.com/${username}`)
-    const $ = cheerio.load(response.data)
-
-    const name = $('span.p-name').text().trim()
-    const avatarUrl = $('img.avatar').attr('src')
-    const description = $('div.p-note').attr('data-bio-text')
-    const followersCount = $(`[href=https://github.com/${username}?tab=followers]>span.text-bold.color-fg-default`).text()
-    const followingCount = $(`[href=https://github.com/${username}?tab=following]>span.text-bold.color-fg-default`).text()
-    console.log('Name :', name)
-    console.log('Avatar URL :', avatarUrl)
-    console.log('Description :', description)
-    console.log(followersCount, 'followers')
-    console.log(followingCount, 'following')
+    try {
+        const userPrompt = new Input({
+            message: 'What is the github username?',
+            initial: 'Fade-ing'
+        })
+        const username = await userPrompt.run()
+        const response = await axios.get(`https://github.com/${username}`)
+        const $ = cheerio.load(response.data)
+    
+        const name = $('span.p-name').text().trim()
+        const avatarUrl = $('img.avatar').attr('src')
+        const description = $('div.p-note').attr('data-bio-text')
+        const followersCount = $(`[href=https://github.com/${username}?tab=followers]>span.text-bold.color-fg-default`).text()
+        const followingCount = $(`[href=https://github.com/${username}?tab=following]>span.text-bold.color-fg-default`).text()
+        console.log('Name :', name)
+        console.log('Avatar URL :', avatarUrl)
+        console.log('Description :', description)
+        console.log(followersCount, 'followers')
+        console.log(followingCount, 'following')
+    } catch (error) {
+        console.log('User not found!')
+    }
 }
 
 githubScrapping()
